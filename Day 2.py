be543012876c0593066024e36dd551cc0c93646b
12Komal12 <145253417+12Komal12@users.noreply.github.com>
2023-10-08 18:38:09 +0530

    4 oct

---
+++ Day 2.py
@@ -0,0 +1,12 @@
+#1st task
+Name= input('Enter name-')
+print(Name)
+
+#2nd task
+firstName=input('Enter First Name-')
+secondName=input('Enter Last Name-')
+print(firstName,secondName,sep=' ', end='\n')
+
+#3rd task
+Name=input('Enter name-')
+print('Welcome',Name,'to the Testing Academy')
