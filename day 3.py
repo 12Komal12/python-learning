261cfbb52b06306e3a2eae13bb4feaa1de0e87a4
12Komal12 <145253417+12Komal12@users.noreply.github.com>
2023-10-09 06:57:46 +0530

    day 3

---
+++ day 3.py
@@ -0,0 +1,43 @@
+
+a=int(input("Enter the number:"))
+print("2",'*',1,'=',2*1)
+print("2",'*',2,'=',2*2)
+print("2",'*',3,'=',2*3)
+print("2",'*',4,'=',2*4)
+print("2",'*',5,'=',2*5)
+print("2",'*',6,'=',2*6)
+print("2",'*',7,'=',2*7)
+print("2",'*',8,'=',2*8)
+print("2",'*',9,'=',2*9)
+print("2",'*',10,'=',2*10)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
+a=int(input("Enter the number:"))
+print("2",'*',a,'=',2*a)
+
