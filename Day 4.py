94820f277acdc2bf7602daaffba2c8eaceee3e31
12Komal12 <145253417+12Komal12@users.noreply.github.com>
2023-10-10 22:00:10 +0530

    9th oct

---
+++ Day 4.py
@@ -0,0 +1,8 @@
+
+a=int(input("enter the number1:"))
+b=int(input("enter the number2:"))
+c=int(input("enter the number3:"))
+d=int(input("enter the number4:"))
+e=int(input("enter the number5:"))
+list=[a,b,c,d,e]
+print(set(list))
