9bab18f9057a9f838be1405384177bda08ec54a8
12Komal12 <145253417+12Komal12@users.noreply.github.com>
2023-10-29 20:46:35 +0530

    Add files via upload
    27th october

--- file 1.py
+++ file 1.py
@@ -1,8 +1,36 @@
-Name = input("Enter your name:\n").lower()
-txt = Name[::-1]
+# 1. Write a Python program to sum all the items in a list
+
+List = [1,4,9,16,25,36,49,64]
+txt = sum(List)
 print(txt)
 
-if Name == txt:
-         print("palindrome possible")
-elif Name != txt:
-         print("palindrome not possible")
+# 2. Write a Python program to multiply all the items in a list
+
+List = [1, 4, 9, 16, 25, 36, 49, 64]
+def multiply(list):
+      chr = 1
+      for i in list:
+          chr *= i
+      return chr
+print(List)
+
+# 3. Write a Python program to get the largest number from a list
+
+List = [1, 4, 9, 16, 25, 36, 49, 64]
+print(max(List))
+
+## 4. Write a Python program to get the smallest number from a list.
+
+List = [1, 4, 9, 16, 25, 36, 49, 64]
+print(min(List))
+
+# 5. Write a Python program to count the number of strings from a given list of strings. The string length is 2 or more and the first and last characters are the same.
+
+List = ['abca','6661' 'xyz', 'aba', '4444']
+def match_words(words):
+    ctr = 0
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+    return ctr
+print(match_words(List))
